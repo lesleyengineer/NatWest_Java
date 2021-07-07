5d5f011975976518ad76da52ac9333b09d3338ad
Vinesh Ghela <vineshghela@gmail.com>
2021-07-07 10:23:08 +0100

    added full calc task

--- src/com/qa/calculatortask/Runner.java
+++ src/com/qa/calculatortask/Runner.java
@@ -5,10 +5,11 @@ public class Runner {
 	// Entry point - MAIN METHOD
 	public static void main(String[] args) {
 		System.out.println(addTwo(10.5423456d, 150.46465d));
-//		System.out.println(addTwo(2354467, 20000));
-//		System.out.println(addTwo(1343, 345646));
-//		System.out.println(subTwo(10, 20));// -10
-//		System.out.println(subTwo(30, 20));
+		System.out.println(subTwo(15.5, 10.4567));
+		System.out.println(mulTwo(235.6, 56));
+		System.out.println(divTwo(35, 4536));
+		System.out.println(divTwo(4, 7));
+		System.out.println(mulTwo(5, 57));
 
 	}// END
 
@@ -17,16 +18,16 @@ public class Runner {
 		return number1 + number2;
 	}
 
-	public static int subTwo(int number1, int number2) {
+	public static double subTwo(double number1, double number2) {
 		return number1 - number2;
 	}
 
-	public static int mulTwo() {
-		return 0;
+	public static double mulTwo(double number1, double number2) {
+		return number1 * number2;
 	}
 
-	public static int divTwo() {
-		return 0;
+	public static double divTwo(double number1, double number2) {
+		return number1 / number2;
 	}
 
 }// END
