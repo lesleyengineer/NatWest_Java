b5823c51bd0ed318c175acbfe21e8b69f05e01cb
Vinesh Ghela <vineshghela@gmail.com>
2021-07-08 11:28:49 +0100

    added loops and arrays

--- src/com/qa/loops/Runner.java
+++ src/com/qa/loops/Runner.java
@@ -7,30 +7,54 @@ public class Runner {
 		// for(<some variable>;<some condition>; <incrementer>){
 		// Body/ LOGIC
 		// }
-//		int myNum = 10;
-//
-//		for (int count = 10; count <= myNum;count++) {
-//			System.out.println("Do a press up " + count);
-//		}
+
+		for (int i = 0; i <= 3; i++) {
+			System.out.println(i);
+		}
+//1a.		first time it runs set count to 1;
+//2.		next it check if count is less than or equal to 5
+//3.		Then if it is it prints the line 
+//4.		And then AFTER increments the value
 
 		// While(condition){
 		// keep doing it
 		// }
 		//
+//		System.out.println("IT continue to run outSIDEEEEEE");
+//		boolean homeTime = false;
+//		int time = 2; // 5pm
+//
+//		while (!homeTime) {  
+//
+//			// its setting homeTime to true;
+//			if (time >= 5) {
+//				homeTime = true;
+//			}
+//			System.out.println("Keep working " + time);
+//			
+//			// increments
+//			time++;
+//
+//		}
+//		while loop check the condition first and only then will run
 
-		boolean homeTime = false;
-		int time = 18; // 5pm
+		// Do While loop WILL RUN ONCE ALWAYS
+//		do{
+//			L O G I C //E
+//		}while(some condition is true)
+//		
 
-		while (!homeTime) {
+		boolean myBool = true;
 
-			// its setting homeTime to true;
-			if (time >= 17) {
-				homeTime = true;
-				return;
-			}
-			System.out.println("Keep working " + time);
-			time++;
+		do {
+			System.out.println("DO WHILE: Dont end the world!");
+			myBool = false;
+		} while (myBool);
 
+		myBool = true;
+		while (myBool) {
+			System.out.println("WHILE: Dont end the world!");
+			myBool = false;
 		}
 
 	}
